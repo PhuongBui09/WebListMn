863de7e98197f8b7e19f3f25553ebb2f24b119da
PhuongBui09 <phuongkaratedo113@gmail.com>
2023-07-01 10:48:17 +0700

    Update date

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// JavaScript (app.js)
 // Retrieve saved data from Local Storage
 const savedData = localStorage.getItem("shoppingList");
 const shoppingList = savedData ? JSON.parse(savedData) : [];
 
 // Debt amount
 let debtAmount = 0;
+
+// Total price
+let totalPrice = 0;
 
 // Delete a specific item
 function deleteItem(index) {
@@ -27,17 +31,109 @@ function deleteAll() {
   renderList();
 }
 
+// Handle edit button click
+function editItem(index) {
+  const editForm = document.getElementById("editForm");
+  const editProductInput = document.getElementById("editProduct");
+  const editPriceInput = document.getElementById("editPrice");
+  const editDateInput = document.getElementById("editDate");
+
+  // Hiển thị form chỉnh sửa và điền thông tin của mục vào form
+  const itemToEdit = shoppingList[index];
+  editProductInput.value = itemToEdit.product;
+  editPriceInput.value = itemToEdit.price;
+  editDateInput.value = itemToEdit.date;
+
+  // Gắn sự kiện submit cho form chỉnh sửa
+  editForm.onsubmit = function (event) {
+    event.preventDefault();
+
+    // Lấy thông tin đã chỉnh sửa từ form
+    const editedProduct = editProductInput.value;
+    const editedPrice = Number(editPriceInput.value);
+    const editedDate = editDateInput.value;
+
+    // Trừ tiền cũ của mục đã chỉnh sửa khỏi tổng tiền hoặc tổng nợ
+    if (itemToEdit.product !== "Đang nợ") {
+      totalPrice -= itemToEdit.price;
+    } else {
+      debtAmount -= itemToEdit.price;
+    }
+
+    // Cập nhật thông tin của mục trong danh sách
+    itemToEdit.product = editedProduct;
+    itemToEdit.price = editedPrice;
+    itemToEdit.date = editedDate;
+
+    // Cộng tiền mới của mục đã chỉnh sửa vào tổng tiền hoặc tổng nợ
+    if (editedProduct !== "Đang nợ") {
+      totalPrice += editedPrice;
+    } else {
+      // Nếu sản phẩm đã chỉnh sửa là "Đang nợ", cập nhật lại tổng nợ
+      debtAmount += editedPrice - itemToEdit.price;
+    }
+
+    // Ẩn form chỉnh sửa và render lại danh sách
+    hideEditForm();
+    renderList();
+  };
+
+  // Hiển thị form chỉnh sửa
+  const editFormContainer = document.getElementById("editFormContainer");
+  editFormContainer.style.display = "block";
+}
+
+// Update item in the shopping list
+function updateItem(index, product, price, date) {
+  const editedItem = shoppingList[index];
+
+  // Trừ tiền cũ của mục đã chỉnh sửa khỏi tổng tiền hoặc tổng nợ
+  if (editedItem.product !== "Đang nợ") {
+    totalPrice -= editedItem.price;
+  } else {
+    debtAmount -= editedItem.price;
+  }
+
+  // Cập nhật thông tin mới cho mục đã chỉnh sửa
+  editedItem.product = product;
+  editedItem.price = price;
+  editedItem.date = date;
+
+  // Cộng tiền mới của mục đã chỉnh sửa vào tổng tiền hoặc tổng nợ
+  if (product !== "Đang nợ") {
+    totalPrice += price;
+  } else {
+    debtAmount += price;
+  }
+
+  // Lưu danh sách đã cập nhật vào Local Storage
+  localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+  // Reload Page sau khi lưu chỉnh sửa
+  reloadPage();
+}
+
+// Hàm tải lại trang
+function reloadPage() {
+  window.location.reload();
+}
+
+// Ẩn form chỉnh sửa
+function hideEditForm() {
+  const editFormContainer = document.getElementById("editFormContainer");
+  editFormContainer.style.display = "none";
+}
+
 // Render shopping list
 function renderList() {
   const list = document.getElementById("list");
   list.innerHTML = "";
 
-  let totalPrice = 0;
+  totalPrice = 0;
 
   // Create an object to store products grouped by date
   const productsByDate = {};
 
-  shoppingList.forEach((item) => {
+  shoppingList.forEach((item, index) => {
     const { product, price, date } = item;
 
     // Format date to "Ngày DD/MM/YYYY"
@@ -45,9 +141,9 @@ function renderList() {
 
     // Group products by date
     if (productsByDate[formattedDate]) {
-      productsByDate[formattedDate].push({ product, price });
+      productsByDate[formattedDate].push({ product, price, index });
     } else {
-      productsByDate[formattedDate] = [{ product, price }];
+      productsByDate[formattedDate] = [{ product, price, index }];
     }
 
     if (product !== "Đang nợ") {
@@ -94,11 +190,19 @@ function renderList() {
         }
       }
 
+      const editButton = document.createElement("button");
+      editButton.textContent = "Chỉnh sửa";
+      editButton.classList.add("edit-button");
+      editButton.addEventListener("click", () => {
+        editItem(item.index);
+      });
+      li.appendChild(editButton);
+
       const deleteButton = document.createElement("button");
       deleteButton.textContent = "Xoá";
       deleteButton.classList.add("delete-button");
       deleteButton.addEventListener("click", () => {
-        deleteItem(index);
+        deleteItem(item.index);
       });
       li.appendChild(deleteButton);
 
@@ -114,8 +218,15 @@ function renderList() {
   });
   totalPriceElement.textContent = `Tổng tiền: ${formattedTotalAmount}`;
 
+  // Lưu danh sách đã cập nhật vào Local Storage
   localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
 }
+
+// Handle edit form cancel button click
+const editFormCancelButton = document.getElementById("editFormCancelButton");
+editFormCancelButton.addEventListener("click", () => {
+  hideEditForm();
+});
 
 // Handle delete all button click
 const deleteAllButton = document.getElementById("deleteAllButton");
@@ -140,6 +251,9 @@ form.addEventListener("submit", (event) => {
     if (product === "Đang nợ") {
       // Update debt amount
       debtAmount += price;
+    } else {
+      // Update total price for other products
+      totalPrice += price;
     }
 
     const currentDate = new Date().toISOString().slice(0, 10);
